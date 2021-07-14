7e29524a89a3af56146da89e247ea5264ab21864
Jacob Levernier <j@adunumdatum.org>
2021-07-14 15:20:24 -0400

    Began implementing paste.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Modal, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -12,27 +12,43 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		console.log('loading plugin');
+		console.log('Loading paste-to-current-indentation');
 
 		await this.loadSettings();
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+		// this.addRibbonIcon('dice', 'Sample Plugin', () => {
+		// 	new Notice('This is a notice!');
+		// });
 
-		this.addStatusBarItem().setText('Status Bar Text');
+		// this.addStatusBarItem().setText('Status Bar Text');
+
+		// this.addCommand({
+		// 	id: 'open-sample-modal',
+		// 	name: 'Open Sample Modal',
+		// 	// callback: () => {
+		// 	// 	console.log('Simple Callback');
+		// 	// },
+		// 	checkCallback: (checking: boolean) => {
+		// 		let leaf = this.app.workspace.activeLeaf;
+		// 		if (leaf) {
+		// 			if (!checking) {
+		// 				new SampleModal(this.app).open();
+		// 			}
+		// 			return true;
+		// 		}
+		// 		return false;
+		// 	}
+		// });
 
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
+			id: 'paste-text-to-current-indentation',
+			name: 'Paste text to current indentation',
 			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
+				let view = this.app.workspace.activeLeaf.view;
+				if (view) {
+					if (!checking && view instanceof MarkdownView) {
+						console.log('Pasting...');
+						this.pasteText(view);
 					}
 					return true;
 				}
@@ -40,22 +56,63 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// this.addCommand({
+		// 	id: 'paste-blockquote-to-current-indentation',
+		// 	name: 'Paste blockquote to current indentation',
+		// 	// callback: () => {
+		// 	// 	console.log('Simple Callback');
+		// 	// },
+		// 	checkCallback: (checking: boolean) => {
+		// 		let leaf = this.app.workspace.activeLeaf;
+		// 		if (leaf) {
+		// 			if (!checking) {
+		// 				new SampleModal(this.app).open();
+		// 			}
+		// 			return true;
+		// 		}
+		// 		return false;
+		// 	}
+		// });
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// this.registerCodeMirror(async (cm: CodeMirror.Editor) => {
+		// 	console.log('codemirror', cm);
+		// 	console.log('Permissions:', navigator.permissions);
+		// 	var clipboardText = await navigator.clipboard
+		// 	console.log('Text:', clipboardText);
+		// });
 
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
+
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-		console.log('unloading plugin');
+	async pasteText(view: MarkdownView) {
+		const clipboardText = await navigator.clipboard.readText();
+		if (clipboardText !== '') {
+			console.log(95, clipboardText);
+			const currentLineNumber = view.sourceMode.editor.getCursor().line;
+			const currentLineText = view.sourceMode.editor.getLine(currentLineNumber);
+			const leadingWhitespace = currentLineText.match(/^(\s*).*/)[1];
+			console.log(97, leadingWhitespace);
+			const clipboardTextIndented = clipboardText.replaceAll(
+				/\n/, `\n${leadingWhitespace}`);
+			console.log(102, clipboardTextIndented);
+			view.sourceMode.editor.setLine(
+				currentLineNumber, currentLineText + clipboardTextIndented);
+
+			return;
+		}
+
+		console.log('Nothing to paste from the clipboard!')
 	}
+
+	// onunload() {
+	// 	console.log('unloading plugin');
+	// }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
