89cef2d632015451c9f56cc614086803c4dc81d7
Jacob Levernier <j@adunumdatum.org>
2021-07-15 16:27:44 -0400

    Updated documentation.

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-import { App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { 
+	App, MarkdownView, Notice, Plugin, PluginSettingTab, Setting 
+} from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PastetoIndentationPluginSettings {
+	blockquotePrefix: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PastetoIndentationPluginSettings = {
+	blockquotePrefix: '> '
 }
-
-const quoteMarker = '> '
 
 // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping,
 // which, as a code snippet, is in the public domain, per 
@@ -19,37 +19,13 @@ function escapeRegExp(string: string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PastetoIndentationPlugin extends Plugin {
+	settings: PastetoIndentationPluginSettings;
 
 	async onload() {
-		console.log('Loading paste-to-current-indentation');
-
 		await this.loadSettings();
 
-		// this.addRibbonIcon('dice', 'Sample Plugin', () => {
-		// 	new Notice('This is a notice!');
-		// });
-
-		// this.addStatusBarItem().setText('Status Bar Text');
-
-		// this.addCommand({
-		// 	id: 'open-sample-modal',
-		// 	name: 'Open Sample Modal',
-		// 	// callback: () => {
-		// 	// 	console.log('Simple Callback');
-		// 	// },
-		// 	checkCallback: (checking: boolean) => {
-		// 		let leaf = this.app.workspace.activeLeaf;
-		// 		if (leaf) {
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-		// 			return true;
-		// 		}
-		// 		return false;
-		// 	}
-		// });
+		this.addSettingTab(new SettingTab(this.app, this));
 
 		this.addCommand({
 			id: 'paste-text-to-current-indentation',
@@ -75,8 +51,7 @@ export default class MyPlugin extends Plugin {
 				let view = this.app.workspace.activeLeaf.view;
 				if (view) {
 					if (!checking && view instanceof MarkdownView) {
-						console.log('Pasting quote...');
-						this.pasteText(view, quoteMarker);
+						this.pasteText(view, this.settings.blockquotePrefix);
 					}
 					return true;
 				}
@@ -91,60 +66,25 @@ export default class MyPlugin extends Plugin {
 				let view = this.app.workspace.activeLeaf.view;
 				if (view) {
 					if (!checking && view instanceof MarkdownView) {
-						console.log('Toggling quote...');
-						this.toggleQuote(view, quoteMarker);
+						this.toggleQuote(view, this.settings.blockquotePrefix);
 					}
 					return true;
 				}
 				return false;
 			}
 		});
-
-		// this.addCommand({
-		// 	id: 'paste-blockquote-to-current-indentation',
-		// 	name: 'Paste blockquote to current indentation',
-		// 	// callback: () => {
-		// 	// 	console.log('Simple Callback');
-		// 	// },
-		// 	checkCallback: (checking: boolean) => {
-		// 		let leaf = this.app.workspace.activeLeaf;
-		// 		if (leaf) {
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-		// 			return true;
-		// 		}
-		// 		return false;
-		// 	}
-		// });
-
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// this.registerCodeMirror(async (cm: CodeMirror.Editor) => {
-		// 	console.log('codemirror', cm);
-		// 	console.log('Permissions:', navigator.permissions);
-		// 	var clipboardText = await navigator.clipboard
-		// 	console.log('Text:', clipboardText);
-		// });
-
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	async pasteText(view: MarkdownView, prefix: string = '') {
 		const clipboardText = await navigator.clipboard.readText();
 		if (clipboardText !== '') {
-			console.log(95, clipboardText);
 			const currentCursor = view.sourceMode.editor.getCursor();
-			const currentLineText = view.sourceMode.editor.getLine(currentCursor.line);
+			const currentLineText = view.sourceMode.editor.getLine(
+				currentCursor.line
+			);
 			const leadingWhitespace = currentLineText.match(/^(\s*).*/)[1];
-			console.log(97, leadingWhitespace);
 			const clipboardTextIndented = clipboardText.replaceAll(
 				/\n/g, '\n' + leadingWhitespace + prefix);
-			console.log(102, clipboardTextIndented);
 			const replacementText = prefix + 
 					clipboardTextIndented;
 			view.sourceMode.editor.replaceSelection(
@@ -158,16 +98,13 @@ export default class MyPlugin extends Plugin {
 		new Notice('The clipboard is currently empty.');
 	}
 
-	async toggleQuote(view: MarkdownView, prefix: string = quoteMarker) {
+	async toggleQuote(
+		view: MarkdownView,
+		prefix: string = this.settings.blockquotePrefix
+	) {
 		const escapedPrefix = escapeRegExp(prefix);
 		const currentSelectionStart = view.sourceMode.editor.getCursor('from');
 		const currentSelectionEnd = view.sourceMode.editor.getCursor('end');
-		console.log(
-			155, 
-			currentSelectionStart, 
-			currentSelectionEnd, 
-			view.sourceMode.editor.getLine(currentSelectionEnd.line)
-		);
 		
 		const replacementRange = [
 			{line: currentSelectionStart.line, ch: 0},
@@ -182,8 +119,6 @@ export default class MyPlugin extends Plugin {
 			replacementRange[1]
 		).split('\n');
 
-		console.log(165, fullSelectedLines);
-
 		const leadingWhitespaces = fullSelectedLines.map(
 			(e: string) => {
 				const whitespaceMatch = e.match(new RegExp(`^(\\s*)`));
@@ -193,9 +128,6 @@ export default class MyPlugin extends Plugin {
 		const minLeadingWhitespaceLength = Math.min(
 			...leadingWhitespaces.map((e: string) => e.length)
 		);
-
-		console.log(192, leadingWhitespaces, minLeadingWhitespaceLength);
-		console.log(198, fullSelectedLines);
 
 		// Determine whether *every* line is Prefixed or not. If not, we will
 		// add the prefix to every line; if so, we will remove it from every line.
@@ -230,8 +162,6 @@ export default class MyPlugin extends Plugin {
 				}
 			}
 		}
-
-		console.log(225, fullSelectedLines);
 
 		view.sourceMode.editor.replaceRange(
 			fullSelectedLines.join('\n'),
@@ -269,26 +199,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+	plugin: PastetoIndentationPlugin;
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PastetoIndentationPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -298,17 +212,24 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Paste to Current Indentation'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Blockquote Prefix')
+			.setDesc(
+				'Markdown syntax to signify that a line is part of a blockquote.'
+			)
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
+				.setPlaceholder('>•')
+				.setValue(
+					this.plugin.settings.blockquotePrefix === DEFAULT_SETTINGS.blockquotePrefix ?
+						'' :
+						this.plugin.settings.blockquotePrefix
+				)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.blockquotePrefix = value !== '' 
+						? value :
+						DEFAULT_SETTINGS.blockquotePrefix;
 					await this.plugin.saveSettings();
 				}));
 	}
