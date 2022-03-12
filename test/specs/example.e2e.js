b8df19cdeaeb5f7091fa45dd33f44c41221a906e
Jacob Levernier <j@adunumdatum.org>
2022-03-12 11:02:23 -0500

    Working on implementing tests.

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,5 +1,7 @@
 const { assert } = require("chai");
 const fs = require("fs");
+const path = require('path');
+
 
 // Note: Supported keyboard key names can be found here:
 // https://w3c.github.io/webdriver/webdriver-spec.html#keyboard-actions
@@ -25,18 +27,12 @@ describe("Plugin", function () {
 
         // Disable safemode and enable the plugin:
         await browser.execute(
-            "app.plugins.setEnable(true);app.plugins.enablePlugin('obsidian-carry-forward')"
+            "app.plugins.setEnable(true);app.plugins.enablePlugin('obsidian-paste-to-current-indentation')"
         );
 
         // Dismiss warning model:
         await browser.$(".modal-button-container button:last-child").click();
         await sleep(0.5);
-
-        // Change the default settings text:
-        await (await browser.$$('.vertical-tab-header-group')[1]).$('.vertical-tab-nav-item').click();
-        await browser.$('.setting-item-control').click();
-        await browser.keys('here');
-        await sleep(1);
 
         // Exit settings:
         await browser.$(".modal-close-button").click();
@@ -65,32 +61,18 @@ describe("Plugin", function () {
     it("adds commands", async function () {
         await browser.$('.view-content').keys(['Control', 'p']);
         await sleep(0.5);
-        await browser.$(".prompt-input").keys("Carry Forward");
+        await browser.$(".prompt-input").keys("Paste to Current Indentation");
         const commands = (await browser.$$(".suggestion-item")).length;
-        assert(commands === 12, 'Wrong number of commands in Command Palette');
+        assert(commands === 10, 'Wrong number of commands in Command Palette');
     });
 
-    it("correctly copies the contents of all four lines in a block", async function () {
+    it("correctly pastes in text mode", async function () {
         const testText = `- Lorem ipsum 1
     - Lorem ipsum 2
         - Lorem ipsum 3
-    - Lorem ipsum 4
-`;
-        await browser.$('.view-content').click();
-        await browser.$('.view-content').keys(testText);
+    - Lorem ipsum 4`;
 
-        // Select text:
-        await browser.$('.CodeMirror-line').keys(['ArrowUp']);
-        await browser.$('.CodeMirror-line').keys(['End']);
-        await browser.$('.CodeMirror-line').keys(['Shift', 'ArrowUp']);
-        await browser.$('.CodeMirror-line').keys(['Shift', 'ArrowUp']);
-        await browser.$('.CodeMirror-line').keys(['Shift', 'ArrowUp']);
-        await browser.$('.CodeMirror-line').keys(['Shift', 'Home']);
-
-        await browser.$('.view-content').keys(['Control', 'p']);
-        await sleep(1);
-        await browser.$(".prompt-input").keys("Carry Forward: Copy selection with each line linked to its copied source (default link text)");
-        await browser.$(".suggestion-item").click();
+        fs.writeFileSync(path.join('test', 'empty_vault', 'Untitled.md'), testText);
         await sleep(1);
 
         // Check the contents of the clipboard:
@@ -100,16 +82,73 @@ describe("Plugin", function () {
 
         await $('.view-content').click({ button: "right" });
         await sleep(1);
+        await $$('.menu-item')[2].click(); // "Cut"
+
+        await browser.$('.view-content').keys(['Control', 'p']);
+        await sleep(1);
+        await browser.$(".prompt-input").keys("Paste to Current Indentation: Set Paste Mode to Text");
+        await browser.$(".suggestion-item").click();
+        await sleep(1);
+
+        await $('.view-content').click({ button: "right" });
+        await sleep(1);
         await $$('.menu-item')[4].click(); // "Paste"
+
+        await sleep(2);
 
         const pastedText = await $('.view-content').getText();
 
-        console.log(101, pastedText);
+        console.log('Input text:')
+        console.log(testText)
+        console.log('Pasted text:')
+        console.log(pastedText)
 
         const testTextSplit = testText.split(/\r?\n/);
         const pastedTextSplit = pastedText.split(/\r?\n/);
 
-        assert(pastedTextSplit[0 + 1].startsWith(testTextSplit[0]), `Line 0 ("${testTextSplit[0]}") is not the same ("${pastedTextSplit[0 + 1]}")`);
-        assert(pastedTextSplit[0 + 1].endsWith('|here]])'), `Line 0 ("${pastedTextSplit[0 + 1]}") does not end with "|here]])")`);
+        assert(pastedTextSplit[1].trim() == testTextSplit[1].trim(), `Line 1 ("${pastedTextSplit[1]}") is not as expected ("${testTextSplit[1]}")`);
+    });
+
+    it("correctly pastes in text blockquote mode", async function () {
+        const testText = `- Lorem ipsum 1
+    - Lorem ipsum 2
+        - Lorem ipsum 3
+    - Lorem ipsum 4`;
+
+        fs.writeFileSync(path.join('test', 'empty_vault', 'Untitled.md'), testText);
+        await sleep(1);
+
+        // Check the contents of the clipboard:
+        await $('.view-content').click({ button: "right" });
+        await sleep(1);
+        await $$('.menu-item')[4].click(); // "Select all"
+
+        await $('.view-content').click({ button: "right" });
+        await sleep(1);
+        await $$('.menu-item')[2].click(); // "Cut"
+
+        await browser.$('.view-content').keys(['Control', 'p']);
+        await sleep(1);
+        await browser.$(".prompt-input").keys("Paste to Current Indentation: Set Paste Mode to Text (Blockquote)");
+        await browser.$(".suggestion-item").click();
+        await sleep(1);
+
+        await $('.view-content').click({ button: "right" });
+        await sleep(1);
+        await $$('.menu-item')[4].click(); // "Paste"
+
+        await sleep(2);
+
+        const pastedText = await $('.view-content').getText();
+
+        console.log('Input text:')
+        console.log(testText)
+        console.log('Pasted text:')
+        console.log(pastedText)
+
+        const testTextSplit = testText.split(/\r?\n/);
+        const pastedTextSplit = pastedText.split(/\r?\n/);
+
+        assert(pastedTextSplit[1].trim() == `> ${testTextSplit[1]}`.trim(), `Line 1 ("${pastedTextSplit[1]}") is not as expected ("> ${testTextSplit[1]}")`);
     });
 });
